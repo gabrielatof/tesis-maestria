e35c7ce5e9ca7e59909bb0dc61ef119a9da7c8ba
Gabriela Montserrat Torres-Fernández <131803135+gabrielatof@users.noreply.github.com>
2025-05-06 19:30:01 -0600

    Create microbiota-analysis-q2.py
    Código utilizado para analizar la microbiota intratumoral mediante Qiime-2

---
+++ microbiota-analysis-q2.py
@@ -0,0 +1,59 @@
+conda activate qiime2-amplicon-2024.10 #activar enviroment de qiime2
+
+####      Importar datos
+
+#cd secuencias-humano/ #cambiar a la carpeta donde están las secuencias
+gzip *.fastq #transformar las secuencias para poderlas leer
+
+qiime tools import \
+  --type 'SampleData[PairedEndSequencesWithQuality]' \
+  --input-path /home/gaby_torres/Documents/tesis-maestria/qiime2/secuencias-humano/ \
+  --input-format CasavaOneEightSingleLanePerSampleDirFmt \ 
+  --output-path demux-paired-end.qza
+
+#Para poder visualizar las secuencias
+
+qiime demux summarize \
+  --i-data demux-paired-end.qza \
+  --o-visualization demux-paired-end.qzv
+
+#subir la tabla de metadatos
+qiime metadata tabulate \ 
+    --m-input-file metadata.tsv \
+    --o-visualization metadata.qzv
+
+
+####      Control de calidad
+
+
+qiime dada2 denoise-paired \
+  --i-demultiplexed-seqs demux-paired-end.qza \
+  --p-trunc-len-f 250 \
+  --p-trim-left-r 1 \
+  --p-trunc-len-r 240 \
+  --o-representative-sequences rep-seqs.qza \
+  --o-table table.qza \
+  --o-denoising-stats denosing-stats.qza
+
+#este proceso genera 3 archivos, los cual vamos a generar su .qzv para poderlos visualizar
+
+#resultados de filtrado
+qiime metadata tabulate \
+  --m-input-file denosing-stats.qza \
+  --o-visualization denosing-stats.qzv 
+
+#asv's que funcionara para la asignacion
+qiime feature-table summarize \
+  --i-table table.qza \
+  --m-sample-metadata-file metadata.tsv \
+  --o-visualization table.qzv
+
+#tabla de asv y abundancias
+qiime feature-table tabulate-seqs \
+  --i-data rep-seqs.qza \
+  --o-visualization rep-seqs.qzv
+
+####      Asignación taxonómica
+
+
+
